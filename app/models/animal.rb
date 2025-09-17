7e211895cc6c70320b89a95f990c36d4ca2ba642
Madelyn Romero <m.romero013@gmail.com>
2025-09-16 19:04:34 -0600

    Set up enums for species, gender, status

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,3 +1,7 @@
 class Animal < ApplicationRecord
     validates :name, :lhs_id, :species, :status, presence: true
+
+    enum :species, { dog: 0, cat: 1, small_and_furry: 2 }
+    enum :gender, { male: 0, female: 1 }
+    enum :status, { new_to_shelter: 0, adoptable: 1, adopted: 2, returned: 3 }
 end
