eb48e0dd38d9f2fa7bf02e70fbed1ad9f494c910
Madelyn Romero <m.romero013@gmail.com>
2025-09-23 19:17:15 -0600

    Fix: change single quotes to double

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -12,12 +12,12 @@ class AnimalsController < ApplicationController
         wait = Selenium::WebDriver::Wait.new(timeout: 10)
         wait.until { driver.find_element(:css, ".animal") }
 
-        all_animals = driver.find_elements(css: '.animal')
+        all_animals = driver.find_elements(css: ".animal")
 
         all_animals.each do |animal|
-            if animal.attribute('data-species') == "Dog"
+            if animal.attribute("data-species") == "Dog"
                 name = animal.find_element(css: ".name").text
-                lhs_id = animal.find_element(css: 'a').attribute("href").delete_prefix(BASE_ANIMAL_URL)
+                lhs_id = animal.find_element(css: "a").attribute("href").delete_prefix(BASE_ANIMAL_URL)
                 gender = animal.attribute("data-sex").downcase
 
                 new_animal = Animal.create!(name:  name, lhs_id: lhs_id, gender: gender)
