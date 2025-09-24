e6030614b224eda55db2f24495afc4f6051e66f4
Madelyn Romero <m.romero013@gmail.com>
2025-09-23 18:59:40 -0600

    Set up initial driver navigating to LHS animals page

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,4 +1,10 @@
 class AnimalsController < ApplicationController
     def index
+        options = Selenium::WebDriver::Chrome::Options.new
+        options.add_argument("--headless")
+
+        driver = Selenium::WebDriver.for :chrome, options: options
+
+        driver.navigate.to("https://www.longmonthumane.org/animals/")
     end
 end
