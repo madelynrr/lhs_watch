8f2556b4ec5bd3f25ac3154af9b63150f620abd2
Madelyn Romero <m.romero013@gmail.com>
2025-10-13 13:44:15 -0600

    Fix single string typo

--- app/services/lhs_webscraper.rb
+++ app/services/lhs_webscraper.rb
@@ -12,14 +12,14 @@ class LhsWebscraper
         wait = Selenium::WebDriver::Wait.new(timeout: 10)
         wait.until { driver.find_element(:css, ".animal") }
 
-        all_animals = driver.find_elements(css: '.animal')
+        all_animals = driver.find_elements(css: ".animal")
 
         fetched_dog_info = []
         all_animals.each do |animal|
-            if animal.attribute('data-species') == "Dog"
+            if animal.attribute("data-species") == "Dog"
                 new_dog = {}
                 new_dog["name"] = animal.find_element(css: ".name").text
-                new_dog["lhs_id"] = animal.find_element(css: 'a').attribute("href").delete_prefix(BASE_ANIMAL_URL)
+                new_dog["lhs_id"] = animal.find_element(css: "a").attribute("href").delete_prefix(BASE_ANIMAL_URL)
                 new_dog["gender"] = animal.attribute("data-sex").downcase
                 # new_dog["age"] = animal.text
                 new_dog["image_url"] = animal.find_elements(tag_name: "img").first.attribute("src")
